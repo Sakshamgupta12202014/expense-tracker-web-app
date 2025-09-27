5127f810da8eb562d1ab1573cdf28b3043c80c81
Sakshamgupta12202014 <gupta.saksham0401@gmail.com>
2025-09-28 00:51:19 +0530

    refactor: enhance header navigation with icons and improve layout styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,54 @@ import React from "react";
 import "./Header.css";
 import { useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Import icons from @fortawesome/free-solid-svg-icons for solid icons
+import {
+  faHome,
+  faUser,
+  faWallet,
+  faSignInAlt, // These are solid icons
+  faUserPlus, // These are solid icons
+  faTachometerAlt, // This is also a solid icon
+} from "@fortawesome/free-solid-svg-icons";
 function Header() {
   const isAuthenticated = useSelector((state) => state.isAuthenticated);
   const navItems = [
-    { name: "Home", path: "/" },
-    { name: "Dashboard", path: "/dashboard" , isAuth: isAuthenticated },
-    { name: "Expenses", path: "/expenses" , isAuth: isAuthenticated },
-    { name: "Profile", path: "/profile", isAuth: isAuthenticated },
-    { name: "Login", path: "/login", isAuth: !isAuthenticated },
-    { name: "Sign Up", path: "/signup", isAuth: !isAuthenticated },
+    {
+      name: "Home",
+      path: "/",
+      icon: faHome,
+    },
+    {
+      name: "Dashboard",
+      path: "/dashboard",
+      isAuth: isAuthenticated,
+      icon: faTachometerAlt,
+    },
+    {
+      name: "Expenses",
+      path: "/expenses",
+      isAuth: isAuthenticated,
+      icon: faWallet,
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      isAuth: isAuthenticated,
+      icon: faUser,
+    },
+    {
+      name: "Login",
+      path: "/login",
+      isAuth: !isAuthenticated,
+      icon: faSignInAlt,
+    },
+    {
+      name: "Sign Up",
+      path: "/signup",
+      isAuth: !isAuthenticated,
+      icon: faUserPlus,
+    },
   ];
 
   return (
@@ -28,7 +66,8 @@ function Header() {
                       isActive ? "nav-link active" : "nav-link"
                     }
                   >
-                    {item.name}
+                    {item.icon && <FontAwesomeIcon icon={item.icon} />}
+                    <span className="nav-item-name">{item.name}</span>
                   </NavLink>
                 </li>
               );
